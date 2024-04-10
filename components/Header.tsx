7fb36c9aeeda7be989dea9a4fd1bde2443747b32
luisrrv <rodluisr@gmail.com>
2024-04-10 17:55:48 +0900

    landing page update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,43 +1,22 @@
-import NextLogo from "./NextLogo";
 import SupabaseLogo from "./SupabaseLogo";
 
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
+        {/* TODO: Replace SupabaseLogo with  FootyPulseLogo */}
+        <SupabaseLogo /> 
       </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
-      </p>
+      {/* <h1 className="sr-only">Supabase and Next.js Starter Template</h1> */}
+      <div>
+        <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center mb-2">
+          Get updates and stats of your favorite football players.
+        </p>
+        <p className="text-xs lg:text-xs tracking-wider opacity-80 !leading-tight mx-auto max-w-xl text-center">
+          Stay informed with pulse messages showcasing the latest stats, news, and/or insights, all in one place.
+        </p>
+      </div>
+      {/* {TODO: image here of a phone showing how a pulse message with stats of your favorite palyer(s) would look like } */}
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
   );
