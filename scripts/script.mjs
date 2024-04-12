22eeece6f7b632d4ecc5a0ae572016bd67cddd56
luisrrv <rodluisr@gmail.com>
2024-04-12 18:53:02 +0900

    script with line bot habdler

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,7 +1,36 @@
 import axios from 'axios';
-// import pkg from '../utils/supabase/script_requests.js';
-// const { getUsers, getFollowed } = pkg;
+import * as line from "@line/bot-sdk";
 import { getUsers, getFollowed } from '../utils/supabase/script_requests.js';
+
+export default async function lineBotHandler(user_id, message) {
+  const config = {
+    // channelAccessToken: process.env.NEXT_PUBLIC_LINE_CHANNEL_ACCESS_TOKEN || "",
+    // channelSecret: process.env.NEXT_PUBLIC_LINE_CHANNEL_SECRET || "",
+    channelAccessToken: "Mj3t5Z9OKuHvKzVJ1RrtVSVl5IC8wPPG2j1QstLlRyemu9WbnQPXmk6G2kbR5+j8jlOHRY9in+pinlq+qN8zypmrOg09Lguu7jMYBhP2ZewLMblDsh32O8szK5LGdIkZMCCUbfALq4HwnnHHoLsbxgdB04t89/1O/w1cDnyilFU=",
+    channelSecret: "050b9f7910e6c87249e09023d18f1523"
+  };
+
+  const client = new line.Client(config);
+
+  try {
+    console.log("~~~~~~~~~~~~~~~~~~~~~~", user_id, message);
+    // const { message, user_id } = req.body;
+    
+    if (!user_id || !message) {
+        return res.status(400).json({ error: "Invalid request data." });
+    }
+
+    await client.pushMessage(user_id, {
+        type: "text",
+        text: message,
+    });
+
+    res.status(200).json({ message: `Message: "${message}" has been sent.` });
+  } catch (e) {
+    console.error("Line API Error:", e);
+    res.status(500).json({ error: "Failed to send message." });
+  }
+}
 
 async function sendData(user, playersData) {
   try {
@@ -10,10 +39,13 @@ async function sendData(user, playersData) {
       return;
     }
     console.log(`Sending data to user ${user.line_id}`, playersData);
-    const lineRes = await axios.post("pages/api/linebot", {
-      message: "This is a test message sent from FootyPulse.",
-      user_id: user.line_id
-    });
+
+    const message =  "This is a test message sent from FootyPulse.";
+    lineBotHandler(user.line_id, message);
+    // const lineRes = await axios.post("pages/api/linebot", {
+    //   message: "This is a test message sent from FootyPulse.",
+    //   user_id: user.line_id
+    // });
     console.log("🚀 ~~~~~ lineRes:", lineRes);
   } catch (error) {
     console.error(`Error sending data to user ${user.id}:`, error);
